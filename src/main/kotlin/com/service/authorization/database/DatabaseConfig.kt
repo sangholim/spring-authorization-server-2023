50a11a4e4bae0002c32cd10d64a02d47a60e0da5
hoya <hybriddevslim@gmail.com>
2023-02-23 23:49:16 +0900

    feat: DatabaseConfig 추가

---
+++ src/main/kotlin/com/service/authorization/database/DatabaseConfig.kt
@@ -0,0 +1,21 @@
+package com.service.authorization.database
+
+import org.springframework.context.annotation.Bean
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType
+
+class DatabaseConfig {
+
+    @Bean
+    fun embeddedDatabase(): EmbeddedDatabase {
+        return EmbeddedDatabaseBuilder()
+                .generateUniqueName(true)
+                .setType(EmbeddedDatabaseType.H2)
+                .setScriptEncoding("UTF-8")
+                .addScript("org/springframework/security/oauth2/server/authorization/oauth2-authorization-schema.sql")
+                .addScript("org/springframework/security/oauth2/server/authorization/oauth2-authorization-consent-schema.sql")
+                .addScript("org/springframework/security/oauth2/server/authorization/client/oauth2-registered-client-schema.sql")
+                .build()
+    }
+}
