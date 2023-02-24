b10e7cd89dd1444bd802b7cd14bf10210f5b846d
hoya <hybriddevslim@gmail.com>
2023-02-24 18:10:58 +0900

    feat: RegisteredClientConfig 추가

---
+++ src/main/kotlin/com/service/authorization/client/RegisteredClientConfig.kt
@@ -0,0 +1,39 @@
+package com.service.authorization.client
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.security.oauth2.core.AuthorizationGrantType
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod
+import org.springframework.security.oauth2.core.oidc.OidcScopes
+import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings
+import java.util.*
+
+@Configuration
+class RegisteredClientConfig {
+
+    @Bean
+    fun registeredClientRepository(jdbcTemplate: JdbcTemplate): RegisteredClientRepository {
+        val repository = JdbcRegisteredClientRepository(jdbcTemplate)
+        val registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("messaging-client")
+                .clientSecret("{noop}secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/messaging-client-oidc")
+                .redirectUri("http://127.0.0.1:8080/authorized")
+                .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .scope("message.read")
+                .scope("message.write")
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .build()
+        repository.findByClientId(registeredClient.clientId) ?: repository.save(registeredClient)
+        return JdbcRegisteredClientRepository(jdbcTemplate)
+    }
+}
