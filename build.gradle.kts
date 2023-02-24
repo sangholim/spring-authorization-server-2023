34f77ff93222da2f41f94df9ecc7a0d4a449f471
hoya <hybriddevslim@gmail.com>
2023-02-24 10:41:45 +0900

    refactor: build.gradle.kts 수정 - postgresql 라이브러리 추가 - liquibase 라이브러리 추가 - jdbc 라이브러리 제거 - jpa 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.0.2"
     id("io.spring.dependency-management") version "1.1.0"
+    id("org.liquibase.gradle") version "2.1.1"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
 }
@@ -21,13 +22,16 @@ dependencies {
     implementation("org.springframework.security:spring-security-oauth2-authorization-server:1.0.1")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.postgresql:postgresql:42.5.4")
+    implementation("org.liquibase:liquibase-core")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     runtimeOnly("com.h2database:h2")
-
+    runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
 }
