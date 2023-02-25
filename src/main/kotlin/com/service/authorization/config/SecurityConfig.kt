915727dec5a27103af87482af88682b876e8d6ea
hoya <hybriddevslim@gmail.com>
2023-02-25 22:14:11 +0900

    refactor: SecurityConfig 수정 - oidc 전용 security 필터 추가 - oidc 전용 endpoint 정의 추가 - 기본 filter 추가 - 기본 filter 에서 oauthLogin 인증 로직  추가

--- src/main/kotlin/com/service/authorization/config/SecurityConfig.kt
+++ src/main/kotlin/com/service/authorization/config/SecurityConfig.kt
@@ -1,53 +1,86 @@
 package com.service.authorization.config
 
-import com.service.authorization.oauth.OAuth2AuthenticationSuccessHandler
-import com.service.authorization.token.TokenProvider
+import com.service.authorization.federatedIdentity.FederatedIdentityConfigurer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint
 import java.util.*
 
 
 @EnableWebSecurity
 @Configuration
-class SecurityConfig(
-        private val tokenProvider: TokenProvider,
-        private val jwtDecoder: JwtDecoder
-) {
+class SecurityConfig {
 
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
-    @Throws(Exception::class)
-    fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        http
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .formLogin().disable()
-                .httpBasic().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("", "/", "/login/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .oauth2Login()
-                .successHandler(OAuth2AuthenticationSuccessHandler(tokenProvider))
-                .loginPage("/templates/index.html")
-                .and()
-                .exceptionHandling()
-                .authenticationEntryPoint { _, response, _ ->
-                    response.sendRedirect("/")
-                }
-                .and()
-                .oauth2ResourceServer {
-                    it.jwt().decoder(jwtDecoder)
-                }
+    @Throws(java.lang.Exception::class)
+    fun authorizationServerSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http)
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer::class.java)
+                .oidc(Customizer.withDefaults()) // Enable OpenID Connect 1.0
+        http // Redirect to the login page when not authenticated from the
+                // authorization endpoint
+                .exceptionHandling { exceptions: ExceptionHandlingConfigurer<HttpSecurity?> ->
+                    exceptions
+                            .authenticationEntryPoint(
+                                    LoginUrlAuthenticationEntryPoint("/login"))
+                } // Accept access tokens for User Info and/or Client Registration
+                .oauth2ResourceServer { it.jwt() }
         return http.build()
     }
 
+
+    @Bean
+    @Order(2)
+    @Throws(java.lang.Exception::class)
+    fun defaultSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+                .authorizeHttpRequests { authorize ->
+                    authorize
+                            .requestMatchers("/assets/**", "/webjars/**", "/login").permitAll()
+                            .anyRequest().authenticated()
+                } // Form login handles the redirect to the login page from the
+                // authorization server filter chain
+                .formLogin(Customizer.withDefaults())
+        http.apply(FederatedIdentityConfigurer())
+        return http.build()
+    }
+
+    @Bean
+    fun authorizationServerSettings(): AuthorizationServerSettings {
+        return AuthorizationServerSettings.builder()
+                .issuer("http://localhost:9090")
+                .authorizationEndpoint("/oauth2/v1/authorize")
+                .tokenEndpoint("/oauth2/v1/token")
+                .tokenIntrospectionEndpoint("/oauth2/v1/introspect")
+                .tokenRevocationEndpoint("/oauth2/v1/revoke")
+                .jwkSetEndpoint("/oauth2/v1/jwks")
+                .oidcUserInfoEndpoint("/connect/v1/userinfo")
+                .oidcClientRegistrationEndpoint("/connect/v1/register")
+                .build()
+    }
+
+    @Bean
+    fun userDetailsService(): UserDetailsService {
+        val userDetails: UserDetails = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build()
+        return InMemoryUserDetailsManager(userDetails)
+    }
 }
