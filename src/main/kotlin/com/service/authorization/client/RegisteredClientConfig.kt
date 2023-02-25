5532efd414ea5f375ec7cf798e6c4f2158693f9a
sangholim <55565835+sangholim@users.noreply.github.com>
2023-02-25 22:15:51 +0900

    feat: OIDC 설정 추가 (#11)
    * feat: FederatedIdentityConfigurer ㅊㅜ가
    
    * feat: RegisteredClientConfig 추가
    - oidc 기반 client 설정
    
    * refactor: SecurityConfig 수정
    - oidc 전용 security 필터 추가
    - oidc 전용 endpoint 정의 추가
    - 기본 filter 추가
    - 기본 filter 에서 oauthLogin 인증 로직  추가

---
+++ src/main/kotlin/com/service/authorization/client/RegisteredClientConfig.kt
@@ -0,0 +1,37 @@
+package com.service.authorization.client
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.oauth2.core.AuthorizationGrantType
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod
+import org.springframework.security.oauth2.core.oidc.OidcScopes
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings
+import java.util.*
+
+
+@Configuration
+class RegisteredClientConfig {
+
+    @Bean
+    fun registeredClientRepository(): RegisteredClientRepository? {
+        val registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("public-client")
+                .clientSecret("{noop}public-secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .redirectUri("http://127.0.0.1:9090/login/oauth2/code/public-client")
+                .redirectUri("http://127.0.0.1:9090/public-client/authorized")
+                .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .scope("message.read")
+                .scope("message.write")
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .build()
+        return InMemoryRegisteredClientRepository(registeredClient)
+    }
+}
