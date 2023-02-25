d46e1bfe858ca66f84681b2195a912eee7f9cd8e
hoya <hybriddevslim@gmail.com>
2023-02-25 14:13:26 +0900

    refactor: SecurityConfig 수정 - 불필요한 객체 생성 로직 제거 - oauth2 인증 성공시 이벤트 로직 추가 - 특정 매체 제외하고는 인증하도록 처리 - jwt 인증 설정 추가

--- src/main/kotlin/com/service/authorization/config/SecurityConfig.kt
+++ src/main/kotlin/com/service/authorization/config/SecurityConfig.kt
@@ -1,33 +1,28 @@
 package com.service.authorization.config
 
-import com.nimbusds.jose.jwk.JWKSelector
-import com.nimbusds.jose.jwk.JWKSet
-import com.nimbusds.jose.jwk.RSAKey
-import com.nimbusds.jose.jwk.source.JWKSource
-import com.nimbusds.jose.proc.SecurityContext
-import com.service.authorization.jose.Jwks
+import com.service.authorization.oauth.OAuth2AuthenticationSuccessHandler
+import com.service.authorization.token.TokenProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.oauth2.jwt.JwtDecoder
-import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationConsentService
-import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService
-import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService
-import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
-import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings
 import org.springframework.security.web.SecurityFilterChain
 import java.util.*
 
 
 @EnableWebSecurity
 @Configuration
-class SecurityConfig {
+class SecurityConfig(
+        private val tokenProvider: TokenProvider,
+        private val jwtDecoder: JwtDecoder
+) {
+
     @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     @Throws(Exception::class)
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http
@@ -38,43 +33,21 @@ class SecurityConfig {
                 .httpBasic().disable()
                 .authorizeHttpRequests()
                 .requestMatchers("", "/", "/login/**").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .oauth2Login()
+                .successHandler(OAuth2AuthenticationSuccessHandler(tokenProvider))
                 .loginPage("/templates/index.html")
-                .defaultSuccessUrl("/console/frame", true)
                 .and()
                 .exceptionHandling()
                 .authenticationEntryPoint { _, response, _ ->
                     response.sendRedirect("/")
                 }
-
+                .and()
+                .oauth2ResourceServer {
+                    it.jwt().decoder(jwtDecoder)
+                }
         return http.build()
     }
 
-    @Bean
-    fun authorizationService(jdbcTemplate: JdbcTemplate, registeredClientRepository: RegisteredClientRepository): OAuth2AuthorizationService {
-        return JdbcOAuth2AuthorizationService(jdbcTemplate, registeredClientRepository)
-    }
-
-    @Bean
-    fun authorizationConsentService(jdbcTemplate: JdbcTemplate, registeredClientRepository: RegisteredClientRepository): OAuth2AuthorizationConsentService? {
-        return JdbcOAuth2AuthorizationConsentService(jdbcTemplate, registeredClientRepository)
-    }
-
-    @Bean
-    fun jwkSource(): JWKSource<SecurityContext?>? {
-        val rsaKey: RSAKey = Jwks.generateRsa()
-        val jwkSet = JWKSet(rsaKey)
-        return JWKSource { jwkSelector: JWKSelector, securityContext: SecurityContext? -> jwkSelector.select(jwkSet) }
-    }
-
-    @Bean
-    fun jwtDecoder(jwkSource: JWKSource<SecurityContext?>?): JwtDecoder? {
-        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource)
-    }
-
-    @Bean
-    fun authorizationServerSettings(): AuthorizationServerSettings? {
-        return AuthorizationServerSettings.builder().build()
-    }
 }
