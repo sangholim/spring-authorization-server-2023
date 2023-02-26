391191a66e1b938222c49e49d53da6bcac2e88fd
hoya <hybriddevslim@gmail.com>
2023-02-26 22:21:45 +0900

    feat: UserNameAndPasswordService 클래스 추가

---
+++ src/main/kotlin/com/service/authorization/user/UserNameAndPasswordService.kt
@@ -0,0 +1,21 @@
+package com.service.authorization.user
+
+import com.service.authorization.userRole.UserRoleService
+import org.slf4j.LoggerFactory
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+
+class UserNameAndPasswordService(
+        private val userService: UserService,
+        private val userRoleService: UserRoleService
+): UserDetailsService {
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+    override fun loadUserByUsername(username: String): UserDetails {
+        val user = userService.getBy(username) ?: throw Exception("없는 회원 입니다")
+        val roles = userRoleService.getAllBy(user.id).map { SimpleGrantedAuthority(it.role) }
+        logger.debug("authenticated user: ${user.email}")
+        return User.builder().username(user.email).password(user.password).authorities(roles).build()
+    }
+}
