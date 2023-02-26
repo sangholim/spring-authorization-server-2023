4d3d4590a89d92915e8d1143347b31ac6b1b3c01
hoya <hybriddevslim@gmail.com>
2023-02-26 22:28:23 +0900

    refactor: FederatedIdentityConfigurer 수정 - oauth2 인증 성공시 유저 생성 및 조회 로직 추가

--- src/main/kotlin/com/service/authorization/federatedIdentity/FederatedIdentityConfigurer.kt
+++ src/main/kotlin/com/service/authorization/federatedIdentity/FederatedIdentityConfigurer.kt
@@ -2,16 +2,21 @@ package com.service.authorization.federatedIdentity
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService
+import org.springframework.security.oauth2.core.oidc.user.OidcUser
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint
 
-class FederatedIdentityConfigurer : AbstractHttpConfigurer<FederatedIdentityConfigurer, HttpSecurity>() {
+class FederatedIdentityConfigurer(
+        private val customerOAuth2UserService: OAuth2UserService<OidcUserRequest, OidcUser>
+) : AbstractHttpConfigurer<FederatedIdentityConfigurer, HttpSecurity>() {
+
     override fun init(http: HttpSecurity) {
-        http
-                .exceptionHandling { exceptions ->
-                    exceptions
-                            .authenticationEntryPoint(
-                                    LoginUrlAuthenticationEntryPoint("/login"))
-                }
-                .oauth2Login()
+        http.exceptionHandling { exceptions ->
+            exceptions.authenticationEntryPoint(LoginUrlAuthenticationEntryPoint(""))
+        }.oauth2Login()
+                .defaultSuccessUrl("/console/frame")
+                .userInfoEndpoint()
+                .oidcUserService(customerOAuth2UserService)
     }
 }
