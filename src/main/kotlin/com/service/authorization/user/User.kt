58c4c38373866791350c82e19c4ba7a3d1409ffd
hoya <hybriddevslim@gmail.com>
2023-02-26 22:20:46 +0900

    feat: User 도메인, Service, Repository 추가

---
+++ src/main/kotlin/com/service/authorization/user/User.kt
@@ -0,0 +1,46 @@
+package com.service.authorization.user
+
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import org.springframework.security.core.userdetails.UserDetails
+import java.util.UUID
+
+/**
+ * 회원 테이블
+ */
+@Entity(name = "user_")
+class User(
+        /**
+         * id
+         */
+        @Id
+        val id: String,
+
+        /**
+         * email
+         */
+        val email: String,
+
+        /**
+         * todo: 암호화 처리
+         */
+        val password: String,
+
+        /**
+         * 활성화 여부
+         */
+        val enabled: Boolean
+) {
+    companion object {
+        /**
+         * 회원 객체 생성
+         */
+        fun of(userDetail: UserDetails): User =
+                User(
+                        id = UUID.randomUUID().toString(),
+                        email = userDetail.username,
+                        password = userDetail.password,
+                        enabled = userDetail.isEnabled
+                )
+    }
+}
---
+++ src/main/kotlin/com/service/authorization/user/UserService.kt
@@ -0,0 +1,14 @@
+package com.service.authorization.user
+
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.stereotype.Service
+
+@Service
+class UserService(
+        private val userRepository: UserRepository
+) {
+
+    fun save(userDetails: UserDetails): User = userRepository.save(User.of(userDetails))
+
+    fun getBy(email: String): User? = userRepository.findByEmail(email)
+}
