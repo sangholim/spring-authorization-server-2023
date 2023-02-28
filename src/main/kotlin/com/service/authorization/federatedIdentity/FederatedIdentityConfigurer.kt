2e4637f03dc28b8f6f3528bee5ff81b8d13644c4
sangholim <55565835+sangholim@users.noreply.github.com>
2023-02-28 14:53:48 +0900

    refactor: oidc 승인 end-point 개선 (#16)
    * feature: FederatedIdentityAuthenticationEntryPoint 추가
    
    * refactor: FederatedIdentityConfigurer 수정
    - 예외 발생시 idp 진입 이벤트 로직 추가

--- src/main/kotlin/com/service/authorization/federatedIdentity/FederatedIdentityConfigurer.kt
+++ src/main/kotlin/com/service/authorization/federatedIdentity/FederatedIdentityConfigurer.kt
@@ -3,17 +3,18 @@ package com.service.authorization.federatedIdentity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService
 import org.springframework.security.oauth2.core.oidc.user.OidcUser
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint
 
 class FederatedIdentityConfigurer(
+        private val clientRegistrationRepository: ClientRegistrationRepository,
         private val customerOAuth2UserService: OAuth2UserService<OidcUserRequest, OidcUser>
 ) : AbstractHttpConfigurer<FederatedIdentityConfigurer, HttpSecurity>() {
 
     override fun init(http: HttpSecurity) {
         http.exceptionHandling { exceptions ->
-            exceptions.authenticationEntryPoint(LoginUrlAuthenticationEntryPoint(""))
+            exceptions.authenticationEntryPoint(FederatedIdentityAuthenticationEntryPoint(clientRegistrationRepository))
         }.oauth2Login()
                 .defaultSuccessUrl("/console/frame")
                 .userInfoEndpoint()
