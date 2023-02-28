f9e3c20028e83fbc68c8a65ab49a220287b06502
hoya <hybriddevslim@gmail.com>
2023-02-28 17:02:31 +0900

    refactor: CustomOAuth2UserService 수정 - 유저 호출 로직 수정 - 회원이 없고 소셜이 없는 경우: 회원, 소셜 데이터 새로 생성 - 회원이 있고 소셜이 없는 경우: 소셜 데이터 새로 생성

--- src/main/kotlin/com/service/authorization/user/CustomOAuth2UserService.kt
+++ src/main/kotlin/com/service/authorization/user/CustomOAuth2UserService.kt
@@ -1,5 +1,6 @@
 package com.service.authorization.user
 
+import com.service.authorization.userFederatedIdentity.UserFederatedIdentityService
 import com.service.authorization.userRole.UserRoleService
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest
@@ -9,10 +10,14 @@ import org.springframework.security.oauth2.core.oidc.user.OidcUser
 
 class CustomOAuth2UserService(
         private val userService: UserService,
-        private val userRoleService: UserRoleService
+        private val userRoleService: UserRoleService,
+        private val userFederatedIdentityService: UserFederatedIdentityService
 ) : OAuth2UserService<OidcUserRequest, OidcUser> {
 
     override fun loadUser(userRequest: OidcUserRequest): OidcUser {
+        // 계정 - 소셜이 없는 경우: 계정 소셜  추가
+        // 계정은 있고 소셜이 없는 경우: 계정에 소셜 연동
+        val subject = userRequest.idToken.subject
         val email = userRequest.idToken.email
         val userDetail = User.withDefaultPasswordEncoder()
                 .username(email)
@@ -21,7 +26,7 @@ class CustomOAuth2UserService(
                 .build()
         val user = userService.getBy(email = email) ?: userService.save(userDetail)
         val userRoles = userRoleService.getAllOrSave(user.id, userDetail.authorities.toList())
-
+        userFederatedIdentityService.getOrSave(subject, user.id, userRequest.clientRegistration.registrationId)
         return DefaultOidcUser(userRoles, userRequest.idToken)
     }
 }
