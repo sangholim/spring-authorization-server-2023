4c240fd4a671d7b529642366d02af32bd6812af2
hoya <hybriddevslim@gmail.com>
2023-03-03 16:05:58 +0900

    feat: TokenCustomizer 추가 - access, id 토큰의 claim 정보를 수정한다

---
+++ src/main/kotlin/com/service/authorization/token/TokenCustomizer.kt
@@ -0,0 +1,30 @@
+package com.service.authorization.token
+
+import com.service.authorization.user.CustomOAuth2UserService
+import org.springframework.security.core.Authentication
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo
+import org.springframework.security.oauth2.core.oidc.endpoint.OidcParameterNames
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer
+
+class TokenCustomizer(
+        private val customOAuth2UserService: CustomOAuth2UserService
+) : OAuth2TokenCustomizer<JwtEncodingContext> {
+    override fun customize(context: JwtEncodingContext) {
+        val subject = context.getPrincipal<Authentication>().name
+        val type = context.tokenType
+        if (OAuth2TokenType.ACCESS_TOKEN.equals(type)) {
+            context.claims.claims { claim ->
+                claim["sub"] = customOAuth2UserService.findUser(subject).id
+            }
+        }
+
+        if (OidcParameterNames.ID_TOKEN == type.value) {
+            val userInfo = OidcUserInfo(customOAuth2UserService.oidcUser(subject))
+            context.claims.claims { claim ->
+                claim.putAll(userInfo.claims)
+            }
+        }
+    }
+}
