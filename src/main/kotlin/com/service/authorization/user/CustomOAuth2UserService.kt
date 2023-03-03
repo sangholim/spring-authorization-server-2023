fb6408e350de7c6506fb41d7d7f6c2b411004d20
hoya <hybriddevslim@gmail.com>
2023-03-03 16:05:23 +0900

    refactor: CustomOAuth2UserService 수정 - username 기반 회원 조회 로직 추가 - oidc user 데이터 생성 로직 추가

--- src/main/kotlin/com/service/authorization/user/CustomOAuth2UserService.kt
+++ src/main/kotlin/com/service/authorization/user/CustomOAuth2UserService.kt
@@ -3,8 +3,10 @@ package com.service.authorization.user
 import com.service.authorization.userFederatedIdentity.UserFederatedIdentityService
 import com.service.authorization.userRole.UserRoleService
 import org.springframework.security.core.userdetails.User
+import org.slf4j.LoggerFactory
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser
 import org.springframework.security.oauth2.core.oidc.user.OidcUser
 
@@ -13,6 +15,8 @@ class CustomOAuth2UserService(
         private val userRoleService: UserRoleService,
         private val userFederatedIdentityService: UserFederatedIdentityService
 ) : OAuth2UserService<OidcUserRequest, OidcUser> {
+
+    private val logger = LoggerFactory.getLogger(this.javaClass)
 
     override fun loadUser(userRequest: OidcUserRequest): OidcUser {
         // 계정 - 소셜이 없는 경우: 계정 소셜  추가
@@ -28,5 +32,25 @@ class CustomOAuth2UserService(
         val userRoles = userRoleService.getAllOrSave(user.id, userDetail.authorities.toList())
         userFederatedIdentityService.getOrSave(subject, user.id, userRequest.clientRegistration.registrationId)
         return DefaultOidcUser(userRoles, userRequest.idToken)
+    }
+
+    fun findUser(subject: String): UserDTO {
+        userService.getById(subject)?.let {
+            return it.toDTO()
+        }
+        val federatedUser = userFederatedIdentityService.getBy(subject) ?: throw Exception("not found subject")
+        return userService.getById(federatedUser.userId)?.toDTO() ?: throw Exception("not found user")
+    }
+
+    fun oidcUser(username: String): Map<String, Any> {
+        val user = findUser(username)
+
+        return OidcUserInfo.builder()
+                .subject(user.id)
+                .preferredUsername(user.id)
+                .email(user.email)
+                .emailVerified(true)
+                .build()
+                .claims
     }
 }
