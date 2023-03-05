adb23c3e6d77d6f8a4ecf5a3f1532a9158405a4f
hoya <hybriddevslim@gmail.com>
2023-03-05 18:14:23 +0900

    refactor: TokenConfig 수정 - 토큰 claim 커스텀 로직 추가

--- src/main/kotlin/com/service/authorization/config/TokenConfig.kt
+++ src/main/kotlin/com/service/authorization/config/TokenConfig.kt
@@ -1,16 +1,21 @@
-package com.service.authorization.token
+package com.service.authorization.config
 
 import com.nimbusds.jose.jwk.JWKSelector
 import com.nimbusds.jose.jwk.JWKSet
 import com.nimbusds.jose.jwk.source.JWKSource
 import com.nimbusds.jose.proc.SecurityContext
 import com.service.authorization.jose.Jwks
+import com.service.authorization.oauth.CustomOAuth2UserService
+import com.service.authorization.token.TokenCustomizer
+import com.service.authorization.token.TokenProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.oauth2.jwt.JwtDecoder
 import org.springframework.security.oauth2.jwt.JwtEncoder
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer
 
 @Configuration
 class TokenConfig {
@@ -35,4 +40,13 @@ class TokenConfig {
 
     @Bean
     fun tokenProvider() = TokenProvider(rsaKey, jwtEncoder())
+
+    /**
+     * 토큰 클레임 커스텀 소스
+     */
+    @Bean
+    fun tokenCustomizer(customerOAuth2UserService: CustomOAuth2UserService):
+            OAuth2TokenCustomizer<JwtEncodingContext> {
+        return TokenCustomizer(customerOAuth2UserService)
+    }
 }
