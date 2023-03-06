1574923bcdbba0bec967b7f76309b6f99682886e
sangholim <55565835+sangholim@users.noreply.github.com>
2023-03-06 21:31:20 +0900

    feat: sessionId 관리 로직 수정 (#32)
    * feat: spring session redis 추가
    - gradle library 추가
    - docker-compose 추가
    - redis.conf 추가
    - application 설정 추가
    
    * refactor: application.yml 수정
    - spring.session 설정 application-local.yml 로 이동
    
    * chore: build.gradle.kts 수정
    - test container 의존성 추가
    
    * refactor: ExtensionConfig 수정
    - redis container 설정 추가
    
    * Test: LoginApiTests 수정
    - 테스트 명세 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.session:spring-session-data-redis")
+
     implementation("org.postgresql:postgresql:42.5.4")
     implementation("org.flywaydb:flyway-core")
 
@@ -43,6 +46,7 @@ dependencies {
     testImplementation("io.kotest:kotest-assertions-core")
     testImplementation("io.kotest:kotest-property")
     testImplementation("org.testcontainers:postgresql:1.17.6")
+    testImplementation("org.testcontainers:testcontainers:1.17.6")
     testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:1.3.4")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
     testImplementation("com.ninja-squad:springmockk:4.0.1")
