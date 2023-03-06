099f6ad3dd43256ffabb64926dc2741ef407f48f
hoya <hybriddevslim@gmail.com>
2023-03-06 21:16:01 +0900

    refactor: ExtensionConfig 수정 - redis container 설정 추가

--- src/test/kotlin/com/service/authorization/config/ExtensionConfig.kt
+++ src/test/kotlin/com/service/authorization/config/ExtensionConfig.kt
@@ -3,8 +3,9 @@ package com.service.authorization.config
 import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.core.extensions.Extension
 import io.kotest.extensions.spring.SpringExtension
+import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.PostgreSQLContainer
-
+import org.testcontainers.utility.DockerImageName
 
 object ExtensionConfig: AbstractProjectConfig() {
     val postgresql = PostgreSQLContainer<Nothing>("postgres:11.1").apply {
@@ -13,8 +14,14 @@ object ExtensionConfig: AbstractProjectConfig() {
         this.withDatabaseName("authorization")
     }
 
+    val redis = GenericContainer(DockerImageName.parse("redis:5.0.3-alpine")).withExposedPorts(6379)
+
     init {
         postgresql.start()
+        redis.start()
+        System.setProperty("spring.data.redis.host", redis.host)
+        System.setProperty("spring.data.redis.port", redis.getMappedPort(6379).toString())
+        System.setProperty("spring.data.redis.password", "")
         System.setProperty("spring.datasource.url", postgresql.jdbcUrl)
         System.setProperty("spring.datasource.username", postgresql.username)
         System.setProperty("spring.datasource.password", postgresql.password)
