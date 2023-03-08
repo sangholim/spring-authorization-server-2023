93e53164f1484ac5cb39136576b3d2567a0aabc6
hoya <hybriddevslim@gmail.com>
2023-03-08 16:50:34 +0900

    test: TestRestTemplateExtension.kt 수정 - oauth2 token endpoint 추가

--- src/test/kotlin/com/service/authorization/client/TestRestTemplateExtension.kt
+++ src/test/kotlin/com/service/authorization/client/TestRestTemplateExtension.kt
@@ -6,7 +6,13 @@ import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.FormHttpMessageConverter
+import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler
+import org.springframework.security.oauth2.core.AuthorizationGrantType
+import org.springframework.security.oauth2.core.endpoint.OAuth2AccessTokenResponse
+import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter
 import org.springframework.util.LinkedMultiValueMap
+import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
 
 fun TestRestTemplate.login(port: Int, username: String = "user", password: String = "password"): ResponseEntity<String> {
@@ -35,4 +41,22 @@ fun TestRestTemplate.oauth2Authorize(sessionId: String, clientId: String, author
             .toUriString()
     val entity = HttpEntity(null, headers)
     return exchange(url, HttpMethod.GET, entity)
+}
+
+
+fun oauth2Token(clientId: String, clientSecret: String?, tokenUrl: String, code: String?, redirectUrl: String?): OAuth2AccessTokenResponse? {
+    val restTemplate = RestTemplate(
+            listOf(FormHttpMessageConverter(), OAuth2AccessTokenResponseHttpMessageConverter()))
+    restTemplate.errorHandler = OAuth2ErrorResponseErrorHandler()
+    val headers = HttpHeaders().apply {
+        this.setBasicAuth(clientId, clientSecret ?: "")
+        this["content-type"] = "application/x-www-form-urlencoded"
+    }
+    val body = LinkedMultiValueMap<String, Any>().apply {
+        this["grant_type"] = AuthorizationGrantType.AUTHORIZATION_CODE.value
+        this["code"] = code
+        this["redirect_uri"] = redirectUrl
+    }
+    val entity = HttpEntity(body, headers)
+    return restTemplate.postForObject(tokenUrl, entity, OAuth2AccessTokenResponse::class.java)
 }
