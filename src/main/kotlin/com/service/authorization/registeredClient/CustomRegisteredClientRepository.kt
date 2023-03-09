3fa084b45feb49cc2bacf60aafad40af6c366eef
hoya <hybriddevslim@gmail.com>
2023-03-09 15:56:33 +0900

    feat: CustomRegisteredClientRepository 추가 - JdbcRegisteredClientRepository 에서 질의 기능을 만드는데 이슈가 있어 새로운 클래스에서 작업할 예정

---
+++ src/main/kotlin/com/service/authorization/registeredClient/CustomRegisteredClientRepository.kt
@@ -0,0 +1,29 @@
+package com.service.authorization.registeredClient
+
+import org.springframework.jdbc.core.JdbcOperations
+import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient
+
+class CustomRegisteredClientRepository(jdbcOperations: JdbcOperations) : JdbcRegisteredClientRepository(jdbcOperations) {
+
+    private val COLUMN_NAMES = ("id, "
+            + "client_id, "
+            + "client_id_issued_at, "
+            + "client_secret, "
+            + "client_secret_expires_at, "
+            + "client_name, "
+            + "client_authentication_methods, "
+            + "authorization_grant_types, "
+            + "redirect_uris, "
+            + "scopes, "
+            + "client_settings,"
+            + "token_settings")
+
+    private val TABLE_NAME = "oauth2_registered_client"
+
+    private val LOAD_REGISTERED_CLIENT_SQL = "SELECT $COLUMN_NAMES FROM $TABLE_NAME "
+
+    fun findBy(): List<RegisteredClient> {
+        return jdbcOperations.query(LOAD_REGISTERED_CLIENT_SQL, registeredClientRowMapper)
+    }
+}
