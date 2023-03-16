d5f8cae5cc1acd2ddc8a8624458d108cb81845d5
sangholim <55565835+sangholim@users.noreply.github.com>
2023-03-16 23:09:09 +0900

    feat: 유저 상세 조회 페이지, api 추가 (#73)
    * refactor: layout/layout.html 수정
    - script block 내부 텍스트 주석
    
    * refactor: UserController 수정
    - 유저 조회 API 추가
    
    * feat: users/detail.html 추가

--- src/main/kotlin/com/service/authorization/user/UserController.kt
+++ src/main/kotlin/com/service/authorization/user/UserController.kt
@@ -3,6 +3,7 @@ package com.service.authorization.user
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
 class UserController(
@@ -13,5 +14,11 @@ class UserController(
     fun getAll(model: Model): String {
         model.addAttribute("users", userService.getAll().map(User::toView))
         return "users/main"
+    }
+
+    @GetMapping("/console/users/{id}")
+    fun get(@PathVariable id: String, model: Model): String {
+        model.addAttribute("user", userService.getById(id)?.toView())
+        return "users/detail"
     }
 }
