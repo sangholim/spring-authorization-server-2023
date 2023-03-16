0948f6bdcb153f0b3744284c4a3b207021e17768
hoya <hybriddevslim@gmail.com>
2023-03-16 23:36:05 +0900

    refactor: UserService 수정 - update 로직 추가

--- src/main/kotlin/com/service/authorization/user/UserService.kt
+++ src/main/kotlin/com/service/authorization/user/UserService.kt
@@ -1,5 +1,6 @@
 package com.service.authorization.user
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Service
 
@@ -15,4 +16,10 @@ class UserService(
     fun getById(id: String): User? = userRepository.findById(id).orElse(null)
 
     fun getAll(): List<User> = userRepository.findAll()
+
+    fun update(id: String, payload: UserUpdatePayload) {
+        val user = userRepository.findByIdOrNull(id) ?: throw Exception("존재하지 않는 회원 id 입니다")
+        if (user.email != payload.email && userRepository.findByEmail(payload.email) != null) throw Exception("사용중인 회원 이메일 입니다")
+        user.update(payload).run(userRepository::save)
+    }
 }
