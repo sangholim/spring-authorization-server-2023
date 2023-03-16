9ea327c9960f2533129f837dd27ce25fc5f9fdb0
hoya <hybriddevslim@gmail.com>
2023-03-16 23:36:46 +0900

    refactor: UserController 수정 - update api 추가

--- src/main/kotlin/com/service/authorization/user/UserController.kt
+++ src/main/kotlin/com/service/authorization/user/UserController.kt
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
 
 @Controller
 class UserController(
@@ -20,5 +21,11 @@ class UserController(
     fun get(@PathVariable id: String, model: Model): String {
         model.addAttribute("user", userService.getById(id)?.toView())
         return "users/detail"
+    }
+
+    @PutMapping("/console/users/{id}")
+    fun update(@PathVariable id: String, payload: UserUpdatePayload): String {
+        userService.update(id, payload)
+        return "redirect:/console/users/$id"
     }
 }
