8a73a3e4da42693421e153df18aaff3693e084df
hoya <hybriddevslim@gmail.com>
2023-03-17 12:15:14 +0900

    refactor: UserController 수정 - 회원 생성 로직 추가

--- src/main/kotlin/com/service/authorization/user/UserController.kt
+++ src/main/kotlin/com/service/authorization/user/UserController.kt
@@ -1,9 +1,11 @@
 package com.service.authorization.user
 
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 
 @Controller
@@ -17,6 +19,10 @@ class UserController(
         return "users/main"
     }
 
+    @GetMapping("/console/users/views/creation")
+    fun getCreateView(): String =
+            "users/creation"
+
     @GetMapping("/console/users/{id}")
     fun get(@PathVariable id: String, model: Model): String {
         model.addAttribute("user", userService.getById(id)?.toView())
@@ -27,5 +33,11 @@ class UserController(
     fun update(@PathVariable id: String, payload: UserUpdatePayload): String {
         userService.update(id, payload)
         return "redirect:/console/users/$id"
+    }
+
+    @PostMapping("/console/users", consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
+    fun save(payload: UserCreationPayload): String {
+        userService.save(payload)
+        return "redirect:/console/users"
     }
 }
