c898e9ca6c7833cd2e15bcf436e73c40821279a8
sangholim <55565835+sangholim@users.noreply.github.com>
2023-03-17 16:45:04 +0900

    feat: 회원 삭제 api 추가 (#80)
    * chore: build.gradle.kts
    - spring jpa kotlin 플러그인 및 설정 추가
    
    * refactor: application-local.yml 수정
    - jpa show-sql 활성화
    
    * refactor: UserRole 수정
    - import library 수정
    
    * refactor: User 수정
    - 소셜 연동 정보 조회 로직 추가
    
    * refactor: UserService 수정
    - id 기준 user 정보 삭제 로직 추가
    
    * refactor: UserController 수정
    - id 리스트 기준 유저 제거 api 추가
    
    * feat: js/users/main.js 추가
    - 회원 삭제 로직 추가
    
    * refactor: users/main.html 수정
    - 회원 스크립트 추가
    - 삭제 버튼 이벤트 추가

---
+++ src/main/resources/static/js/users/main.js
@@ -0,0 +1,15 @@
+function deleteUsers() {
+  const nodes = document.querySelectorAll("input[name='checkId']:checked");
+  const ids = Array.from(nodes).map(x => x.value).join(",");
+  const url = "/console/users?ids=" + ids;
+  const csrf = document.querySelector("input[name='_csrf']").value;
+  var xhr = new XMLHttpRequest();
+  xhr.open("DELETE",url);
+  xhr.onreadystatechange = function() { // listen for state changes
+    if (xhr.readyState == 4) { // when completed we can move away
+        window.location.href="/console/users"
+    }
+  }
+  xhr.setRequestHeader("x-csrf-token", csrf);
+  xhr.send();
+}
