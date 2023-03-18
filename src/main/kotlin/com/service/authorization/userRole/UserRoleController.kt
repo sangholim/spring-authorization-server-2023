b1597a7d04743b097d3c7460c66c4db64446fb38
hoya <hybriddevslim@gmail.com>
2023-03-18 22:18:40 +0900

    feat: UserRoleController 추가 - 회원 권한 조회 추가

---
+++ src/main/kotlin/com/service/authorization/userRole/UserRoleController.kt
@@ -0,0 +1,19 @@
+package com.service.authorization.userRole
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+
+@Controller
+class UserRoleController(
+        private val userRoleService: UserRoleService
+) {
+
+    @GetMapping("/console/users/{userId}/roles")
+    fun getAll(@PathVariable userId: String, model: Model): String {
+        model.addAttribute("userId", userId)
+        model.addAttribute("roles", userRoleService.getAllBy(userId))
+        return "users/roles/main"
+    }
+}
