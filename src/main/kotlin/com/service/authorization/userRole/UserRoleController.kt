3610a7e35e7c7a9a10598c31b81b74db6bdfb988
hoya <hybriddevslim@gmail.com>
2023-03-19 08:59:41 +0900

    refactor: UserRoleController 수정 - 생성 페이지 redirect url 추가

--- src/main/kotlin/com/service/authorization/userRole/UserRoleController.kt
+++ src/main/kotlin/com/service/authorization/userRole/UserRoleController.kt
@@ -16,4 +16,11 @@ class UserRoleController(
         model.addAttribute("roles", userRoleService.getAllBy(userId))
         return "users/roles/main"
     }
+
+    @GetMapping("/console/users/{userId}/roles/views/creation")
+    fun getCreationView(@PathVariable userId: String, model: Model): String {
+        model.addAttribute("userId", userId)
+        model.addAttribute("roles", UserRoleType.values())
+        return "users/roles/creation"
+    }
 }
