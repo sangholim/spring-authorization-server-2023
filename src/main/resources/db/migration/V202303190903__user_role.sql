dc5c799e5819a273947c640da30115109aa47906
hoya <hybriddevslim@gmail.com>
2023-03-19 09:18:49 +0900

    refactor: V202303190903__user_role.sql - 컬럼명 수정

--- src/main/resources/db/migration/V202303190903__user_role.sql
+++ src/main/resources/db/migration/V202303190903__user_role.sql
@@ -1,5 +1,3 @@
--- 회원 권한 테이블
--- name column 추가
-ALTER TABLE user_role ADD COLUMN name varchar(100) NOT NULL;
--- role column 제거
-ALTER TABLE user_role DROP role;
+-- 회원 권한 테이블 컬럼 이름 수정
+
+ALTER TABLE user_role RENAME COLUMN role TO name;
