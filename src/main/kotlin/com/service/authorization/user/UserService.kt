db0b10d0298607dac76dba3c65b664837c029ef4
hoya <hybriddevslim@gmail.com>
2023-03-19 14:48:17 +0900

    refactor: UserService 수정 - fun save(userDetails: UserDetails): User 제거 - 회원 생성 함수 수정 - 회원 업데이트 함수 수정

--- src/main/kotlin/com/service/authorization/user/UserService.kt
+++ src/main/kotlin/com/service/authorization/user/UserService.kt
@@ -2,7 +2,6 @@ package com.service.authorization.user
 
 import com.service.authorization.user.User.Companion.user
 import org.springframework.data.repository.findByIdOrNull
-import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,15 +9,13 @@ class UserService(
         private val userRepository: UserRepository
 ) {
 
-    fun save(userDetails: UserDetails): User = userRepository.save(User.of(userDetails))
-
     fun save(payload: UserCreationPayload): User {
         if (userRepository.findByEmail(payload.email) != null) throw Exception("가입된 이메일입니다")
 
         return user {
             email = payload.email
-            password = payload.password
             enabled = true
+            this.password(payload.password)
         }.run(userRepository::save)
     }
 
@@ -31,7 +28,7 @@ class UserService(
     fun update(id: String, payload: UserUpdatePayload) {
         val user = userRepository.findByIdOrNull(id) ?: throw Exception("존재하지 않는 회원 id 입니다")
         if (user.email != payload.email && userRepository.findByEmail(payload.email) != null) throw Exception("사용중인 회원 이메일 입니다")
-        user.update(payload).run(userRepository::save)
+        User.from(user).update(payload).build().run(userRepository::save)
     }
 
     fun deleteByIds(ids: Set<String>) {
