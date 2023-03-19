840a4e398df1a7df45051c234f8a7e71b4ebdeea
hoya <hybriddevslim@gmail.com>
2023-03-19 09:11:07 +0900

    refactor: UserRoleExtension.kt 수정 - 필드명 수정: role -> name

--- src/main/kotlin/com/service/authorization/userRole/UserRoleExtension.kt
+++ src/main/kotlin/com/service/authorization/userRole/UserRoleExtension.kt
@@ -3,4 +3,4 @@ package com.service.authorization.userRole
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 
 
-fun UserRole.toGrantedAuthority() = SimpleGrantedAuthority(this.role)
+fun UserRole.toGrantedAuthority() = SimpleGrantedAuthority(this.name)
