fb64a87a0df1d3ebaf58d1fc75d07cc2627a94d7
hoya <hybriddevslim@gmail.com>
2023-03-19 13:18:20 +0900

    refactor: UserRoleRepository 수정 - userId 일치 하고, id 포함하는 경우, 삭제 로직 추가

--- src/main/kotlin/com/service/authorization/userRole/UserRoleRepository.kt
+++ src/main/kotlin/com/service/authorization/userRole/UserRoleRepository.kt
@@ -1,8 +1,12 @@
 package com.service.authorization.userRole
 
+import jakarta.transaction.Transactional
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRoleRepository : JpaRepository<UserRole, String> {
 
     fun findByUserId(userId: String): List<UserRole>
+
+    @Transactional
+    fun deleteAllByUserIdAndIdIn(userId: String, ids: Set<String>)
 }
