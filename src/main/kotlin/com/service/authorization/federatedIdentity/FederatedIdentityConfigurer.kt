8229a5c74a725c4cd56e2d2c2c6701deadcabe33
sangholim <55565835+sangholim@users.noreply.github.com>
2023-03-23 22:31:05 +0900

    refactor: 승인 코드 요청시 쿠키로 관리 (#124)
    * feat: CookieUtils 추가
    
    * feat: HttpCookieOauth2AuthorizationRequestRepository 추가
    
    * refactor: FederatedIdentityConfigurer 수정
    - session state-less
    - cookie 기반으로 requestCache 저장
    - 승인 코드 요청 데이터는 쿠키로 관리
    
    * refactor: Oauth2Config 수정
    - federatedIdentityConfig 객체 생성 로직 수정

--- src/main/kotlin/com/service/authorization/federatedIdentity/FederatedIdentityConfigurer.kt
+++ src/main/kotlin/com/service/authorization/federatedIdentity/FederatedIdentityConfigurer.kt
@@ -1,22 +1,31 @@
 package com.service.authorization.federatedIdentity
 
 import com.service.authorization.config.SecurityConstants
+import com.service.authorization.oauth.HttpCookieOauth2AuthorizationRequestRepository
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService
 import org.springframework.security.oauth2.core.oidc.user.OidcUser
+import org.springframework.security.web.savedrequest.CookieRequestCache
 
 class FederatedIdentityConfigurer(
         private val clientRegistrationRepository: ClientRegistrationRepository,
-        private val customerOAuth2UserService: OAuth2UserService<OidcUserRequest, OidcUser>
+        private val customerOAuth2UserService: OAuth2UserService<OidcUserRequest, OidcUser>,
+        private val httpCookieOauth2AuthorizationRequestRepository: HttpCookieOauth2AuthorizationRequestRepository
 ) : AbstractHttpConfigurer<FederatedIdentityConfigurer, HttpSecurity>() {
 
     override fun init(http: HttpSecurity) {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        http.requestCache().requestCache(CookieRequestCache())
         http.exceptionHandling { exceptions ->
             exceptions.authenticationEntryPoint(FederatedIdentityAuthenticationEntryPoint(clientRegistrationRepository))
         }.oauth2Login()
+                .authorizationEndpoint()
+                .authorizationRequestRepository(httpCookieOauth2AuthorizationRequestRepository)
+                .and()
                 .defaultSuccessUrl(SecurityConstants.SUCCESS_URL)
                 .userInfoEndpoint()
                 .oidcUserService(customerOAuth2UserService)
---
+++ src/main/kotlin/com/service/authorization/util/CookieUtil.kt
@@ -0,0 +1,43 @@
+package com.service.authorization.util
+
+import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.util.SerializationUtils
+import java.util.*
+
+
+object CookieUtils {
+    fun getCookie(request: HttpServletRequest, name: String): Cookie? {
+        val cookies: Array<Cookie> = request.cookies
+        return cookies.firstOrNull { it.name == name }
+    }
+
+    fun addCookie(response: HttpServletResponse, name: String, value: String, maxAge: Int) {
+        Cookie(name, value).run {
+            this.path = "/"
+            this.isHttpOnly = true
+            this.maxAge = maxAge
+            response.addCookie(this)
+        }
+    }
+
+    fun deleteCookie(request: HttpServletRequest, response: HttpServletResponse, name: String) {
+        request.cookies.firstOrNull { it.name == name }?.run {
+            this.value = ""
+            this.path = "/"
+            this.maxAge = 0
+            response.addCookie(this)
+        }
+    }
+
+    fun serialize(`object`: Any): String {
+        return Base64.getUrlEncoder()
+                .encodeToString(SerializationUtils.serialize(`object`))
+    }
+
+    fun <T> deserialize(cookie: Cookie, cls: Class<T>): T {
+        return cls.cast(SerializationUtils.deserialize(
+                Base64.getUrlDecoder().decode(cookie.value)))
+    }
+}
