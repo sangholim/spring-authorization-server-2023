d8f97dc48cb7b77c6131fd653879cbb99abf3391
sangholim <55565835+sangholim@users.noreply.github.com>
2023-03-23 16:31:55 +0900

    feat: 회원 승인 데이터 제거 로직 추가 (#119)
    * refactor: 삭제 함수 개선
    - 삭제후, 서버로부터 새로고침 수행
    
    * refactor: /js/authorizations/main.js 수정
    - userId 기준 승인 데이터 제거 로직 추가
    
    * refactor: CustomOAuth2AuthorizationService 수정
    - 함수명 수정
    - userId 기준 승인 데이터 제거
    
    * refactor: OAuth2AuthorizationController 수정
    - 함수명 수정
    - path-variable 수정
    - userId 기준 승인 데이터 제거 api 추가

--- src/main/resources/static/js/authorizations/main.js
+++ src/main/resources/static/js/authorizations/main.js
@@ -8,7 +8,22 @@ const authorization = {
         xhr.open("DELETE",url);
         xhr.onreadystatechange = function() { // listen for state changes
           if (xhr.readyState == 4) { // when completed we can move away
-              window.location.href=`/console/register-clients/${registeredClientId}/authorizations`;
+              window.location.reload(true);
+          }
+        }
+        xhr.setRequestHeader("x-csrf-token", csrf);
+        xhr.send();
+    },
+    deleteByUserId: function (userId) {
+        const nodes = document.querySelectorAll("input[name='checkId']:checked");
+        const ids = Array.from(nodes).map(x => x.value).join(",");
+        const url = `/console/users/${userId}/authorizations?ids=${ids}`;
+        const csrf = document.querySelector("input[name='_csrf']").value;
+        var xhr = new XMLHttpRequest();
+        xhr.open("DELETE",url);
+        xhr.onreadystatechange = function() { // listen for state changes
+          if (xhr.readyState == 4) { // when completed we can move away
+              window.location.reload(true);
           }
         }
         xhr.setRequestHeader("x-csrf-token", csrf);
