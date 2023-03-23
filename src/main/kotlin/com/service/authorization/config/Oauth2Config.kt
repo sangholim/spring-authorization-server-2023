66f3e85cacb51a7f3927a51e6d9befa640fda6e1
hoya <hybriddevslim@gmail.com>
2023-03-24 00:06:16 +0900

    refactor: Oauth2Config 수정 - federatedIdentityConfig 생성 로직 수정

--- src/main/kotlin/com/service/authorization/config/Oauth2Config.kt
+++ src/main/kotlin/com/service/authorization/config/Oauth2Config.kt
@@ -4,19 +4,17 @@ import com.service.authorization.federatedIdentity.FederatedIdentityConfigurer
 import com.service.authorization.oauth.HttpCookieOauth2AuthorizationRequestRepository
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService
 import org.springframework.security.oauth2.core.oidc.user.OidcUser
 
 
 @Configuration
 class Oauth2Config(
-        private val clientRegistrationRepository: ClientRegistrationRepository,
         private val customerOAuth2UserService: OAuth2UserService<OidcUserRequest, OidcUser>
 ) {
     private val httpCookieOauth2AuthorizationRequestRepository = HttpCookieOauth2AuthorizationRequestRepository()
 
     val federatedIdentityConfig: FederatedIdentityConfigurer
-        get() = FederatedIdentityConfigurer(clientRegistrationRepository, customerOAuth2UserService, httpCookieOauth2AuthorizationRequestRepository)
+        get() = FederatedIdentityConfigurer(customerOAuth2UserService, httpCookieOauth2AuthorizationRequestRepository)
 
 }
