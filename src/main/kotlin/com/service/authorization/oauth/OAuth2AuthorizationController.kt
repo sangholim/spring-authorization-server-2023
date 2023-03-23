16748594100fa9793be2581402812bdf1832d78e
hoya <hybriddevslim@gmail.com>
2023-03-23 15:40:40 +0900

    refactor: OAuth2AuthorizationController 수정 - userId 기준 리스트 조회 url 추가

--- src/main/kotlin/com/service/authorization/oauth/OAuth2AuthorizationController.kt
+++ src/main/kotlin/com/service/authorization/oauth/OAuth2AuthorizationController.kt
@@ -11,7 +11,7 @@ class OAuth2AuthorizationController(
 ) {
 
     @GetMapping("/console/register-clients/{id}/authorizations")
-    fun getAll(@PathVariable id: String, model: Model): String {
+    fun getAllByRegisteredClientId(@PathVariable id: String, model: Model): String {
         model.addAttribute("id", id)
         model.addAttribute("authorizations", authorizationService.findByRegisteredClientId(id).map { it.toView() })
         return "register-clients/authorizations/main"
@@ -19,10 +19,17 @@ class OAuth2AuthorizationController(
 
     @DeleteMapping("/console/register-clients/{registeredClientId}/authorizations")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    fun delete(@PathVariable registeredClientId: String, @RequestParam("ids") ids: Set<String>?) {
+    fun deleteByRegisteredClientId(@PathVariable registeredClientId: String, @RequestParam("ids") ids: Set<String>?) {
         if (ids.isNullOrEmpty()) {
             return
         }
         authorizationService.deleteBy(registeredClientId, ids)
+    }
+
+    @GetMapping("/console/users/{id}/authorizations")
+    fun getAllByUserId(@PathVariable id: String, model: Model): String {
+        model.addAttribute("id", id)
+        model.addAttribute("authorizations", authorizationService.findByUserId(id).map { it.toView() })
+        return "users/authorizations/main"
     }
 }
