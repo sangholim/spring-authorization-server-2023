414c854d5b8551d7a850ac94a259365dfef57a49
hoya <hybriddevslim@gmail.com>
2023-03-25 00:05:47 +0900

    refactor: SessionManagementService 수정 - Dsl Builder 적용 - 업데이트 로직 추가

--- src/main/kotlin/com/service/authorization/sessionManagement/SessionManagementService.kt
+++ src/main/kotlin/com/service/authorization/sessionManagement/SessionManagementService.kt
@@ -1,8 +1,10 @@
 package com.service.authorization.sessionManagement
 
+import com.service.authorization.sessionManagement.SessionManagement.Companion.sessionManagement
 import org.springframework.boot.autoconfigure.session.RedisSessionProperties
 import org.springframework.boot.web.servlet.server.Session
 import org.springframework.session.data.redis.RedisSessionRepository
+import java.time.Duration
 
 class SessionManagementService(
         private val sessionManagementRepository: SessionManagementRepository,
@@ -11,9 +13,21 @@ class SessionManagementService(
         redisSessionProperties: RedisSessionProperties
 ) {
     init {
-        val sessionManagement = SessionManagement.of(session.timeout.toMinutes(), redisSessionProperties.flushMode)
-        sessionManagementRepository.save(sessionManagement)
+        val sessionManagement = sessionManagement {
+            this.timeout = session.timeout.toMinutes()
+            this.flushMode = redisSessionProperties.flushMode
+        }
+        if (sessionManagementRepository.count() == 0L) {
+            sessionManagementRepository.save(sessionManagement)
+        }
     }
 
     fun get(): SessionManagement = sessionManagementRepository.findAll().first()
+
+    fun update(payload: SessionManagementUpdatePayload) {
+        SessionManagement.from(get()).update(payload).build().run(sessionManagementRepository::save).run {
+            sessionRepository.setFlushMode(this.flushMode)
+            sessionRepository.setDefaultMaxInactiveInterval(Duration.ofMinutes(this.timeout))
+        }
+    }
 }
