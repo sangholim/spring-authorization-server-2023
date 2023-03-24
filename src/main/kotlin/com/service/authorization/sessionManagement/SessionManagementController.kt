904220a7fd5a40ab4b31bfa133fd63ac04e4c559
hoya <hybriddevslim@gmail.com>
2023-03-25 00:06:05 +0900

    refactor: SessionManagementController 수정 - 업데이트 url 추가

--- src/main/kotlin/com/service/authorization/sessionManagement/SessionManagementController.kt
+++ src/main/kotlin/com/service/authorization/sessionManagement/SessionManagementController.kt
@@ -3,6 +3,7 @@ package com.service.authorization.sessionManagement
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PutMapping
 
 @Controller
 class SessionManagementController(
@@ -10,8 +11,14 @@ class SessionManagementController(
 ) {
 
     @GetMapping("/console/session-management")
-    fun get(model: Model):String {
+    fun get(model: Model): String {
         model.addAttribute("sessionManagement", sessionManagementService.get().toView())
         return "session-management/main"
+    }
+
+    @PutMapping("/console/session-management")
+    fun update(payload: SessionManagementUpdatePayload, model: Model): String {
+        sessionManagementService.update(payload)
+        return "redirect:/console/session-management"
     }
 }
