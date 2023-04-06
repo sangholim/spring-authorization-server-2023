c5c2ab50970feec5fe818d296be4c2a8dccb35fb
hoya <hybriddevslim@gmail.com>
2023-04-07 00:07:30 +0900

    refactor: UserRoleExtension.kt 수정 - 관리자 권한 제외 로직 추가

--- src/main/kotlin/com/service/authorization/userRole/UserRoleExtension.kt
+++ src/main/kotlin/com/service/authorization/userRole/UserRoleExtension.kt
@@ -4,3 +4,5 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority
 
 
 fun UserRole.toGrantedAuthority() = SimpleGrantedAuthority(this.name.toString())
+
+fun List<UserRole>.excludeAdmin() = this.filter { it.name != UserRoleName.ROLE_ADMIN }
