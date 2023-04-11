d4fa7a2ede084b2e5cf1dd2177ca7dbf3cfe6fdd
hoya <hybriddevslim@gmail.com>
2023-04-11 14:42:21 +0900

    refactor: users/federated-identities/main.js 수정 - 유저 제거시 url 경로 수정

--- src/main/resources/static/js/users/federated-identities/main.js
+++ src/main/resources/static/js/users/federated-identities/main.js
@@ -1,7 +1,7 @@
 function deleteUserFederatedIdentities(userId) {
   const nodes = document.querySelectorAll("input[name='checkId']:checked");
   const ids = Array.from(nodes).map(x => x.value).join(",");
-  const url = `/console/users/${userId}/federated-identities?ids=${ids}`;
+  const url = `${contextPath.value}console/users/${userId}/federated-identities?ids=${ids}`;
   const csrf = document.querySelector("input[name='_csrf']").value;
   var xhr = new XMLHttpRequest();
   xhr.open("DELETE",url);
