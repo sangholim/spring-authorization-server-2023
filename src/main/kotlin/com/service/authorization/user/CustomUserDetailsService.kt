15dcd430eade8e0d4eaf04f36dfeae55588cb20b
hoya <hybriddevslim@gmail.com>
2023-04-12 15:09:28 +0900

    refactor: UserNameAndPasswordService 수정 - 클래스 이름 개선 - userId 기준으로 데이터 조회 로직 추가

--- src/main/kotlin/com/service/authorization/user/CustomUserDetailsService.kt
+++ src/main/kotlin/com/service/authorization/user/CustomUserDetailsService.kt
@@ -8,15 +8,21 @@ import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 
-class UserNameAndPasswordService(
+class CustomUserDetailsService(
         private val userService: UserService,
         private val userRoleService: UserRoleService
-): UserDetailsService {
+) : UserDetailsService {
     private val logger = LoggerFactory.getLogger(this.javaClass)
     override fun loadUserByUsername(username: String): UserDetails {
         val user = userService.getBy(username) ?: throw Exception("없는 회원 입니다")
         val roles = userRoleService.getAllBy(user.id).map(UserRole::toGrantedAuthority)
         logger.debug("authenticated user: ${user.email}")
         return User.builder().username(user.id).password(user.password).authorities(roles).build()
+    }
+
+    fun loadUserById(id: String): UserDTO {
+        val user = userService.getById(id) ?: throw Exception("없는 회원 입니다")
+        val roles = userRoleService.getAllBy(user.id).map { it.name.toString() }
+        return user.toDTO(roles)
     }
 }
