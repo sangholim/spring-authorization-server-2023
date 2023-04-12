abc248df6dab7a7457e0bed9d4d4667efcfa8362
hoya <hybriddevslim@gmail.com>
2023-04-12 15:09:49 +0900

    refactor: UserRoleExtension.kt 수정 - excludeAdmin() 제거

--- src/main/kotlin/com/service/authorization/userRole/UserRoleExtension.kt
+++ src/main/kotlin/com/service/authorization/userRole/UserRoleExtension.kt
@@ -4,5 +4,3 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority
 
 
 fun UserRole.toGrantedAuthority() = SimpleGrantedAuthority(this.name.toString())
-
-fun List<UserRole>.excludeAdmin() = this.filter { it.name != UserRoleName.ROLE_ADMIN }
