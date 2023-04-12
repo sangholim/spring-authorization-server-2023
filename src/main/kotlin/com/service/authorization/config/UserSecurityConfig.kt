5d8e8839566e4ea099cd1c6f50a8e21d6677ead1
hoya <hybriddevslim@gmail.com>
2023-04-12 15:08:29 +0900

    refactor: UserNameAndPasswordService 수정 - 클래스 이름 개선 - userId 기준으로 데이터 조회 로직 추가

--- src/main/kotlin/com/service/authorization/config/UserSecurityConfig.kt
+++ src/main/kotlin/com/service/authorization/config/UserSecurityConfig.kt
@@ -1,13 +1,12 @@
 package com.service.authorization.config
 
 import com.service.authorization.oauth.CustomOAuth2UserService
-import com.service.authorization.user.UserNameAndPasswordService
+import com.service.authorization.user.CustomUserDetailsService
 import com.service.authorization.user.UserService
 import com.service.authorization.userFederatedIdentity.UserFederatedIdentityService
 import com.service.authorization.userRole.UserRoleService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService
 import org.springframework.security.oauth2.core.oidc.user.OidcUser
@@ -16,8 +15,8 @@ import org.springframework.security.oauth2.core.oidc.user.OidcUser
 class UserSecurityConfig {
 
     @Bean
-    fun userNameAndPasswordService(userService: UserService, userRoleService: UserRoleService): UserDetailsService =
-            UserNameAndPasswordService(userService, userRoleService)
+    fun customUserDetailsService(userService: UserService, userRoleService: UserRoleService): CustomUserDetailsService =
+            CustomUserDetailsService(userService, userRoleService)
 
     @Bean
     fun customOAuth2UserService(userService: UserService, userRoleService: UserRoleService, userFederatedIdentityService: UserFederatedIdentityService): OAuth2UserService<OidcUserRequest, OidcUser> =
